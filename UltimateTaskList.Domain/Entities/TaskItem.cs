91a6d1accfa51e6bec9d5964136dfe8de6898fc2
Wallace dos Anjos <wallace.anjos@al.infnet.edu.br>
2020-12-06 21:12:44 -0300

    Add project files.

---
+++ UltimateTaskList.Domain/Entities/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UltimateTaskList.Domain.Entities
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+    }
+}
---
+++ UltimateTaskList.Infrastructure/UltimateTaskListDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using UltimateTaskList.Domain.Entities;
+
+namespace UltimateTaskList.Infrastructure
+{
+    public class UltimateTaskListDbContext : DbContext
+    {
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+        public UltimateTaskListDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=UltimateTaskListDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+    }
+}
